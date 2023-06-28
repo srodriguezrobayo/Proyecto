42aeb6f863171b292d385acf471ae511d6f6ca9a
srodriguezrobayo <113036583+srodriguezrobayo@users.noreply.github.com>
2023-06-28 12:03:29 -0500

    Update Segundo_trimestre.py

--- Segundo_trimestre/Segundo_trimestre.py
+++ Segundo_trimestre/Segundo_trimestre.py
@@ -1,2 +1,3 @@
 #Reserve Shoot proyecto SENA todos los trabajos del segundo trimestre
 print("Aqui van todos los trabajos del segundo trimestre")
+#print("Aqui van todos los trabajos del segundo trimestre")
