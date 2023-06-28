f28ec450d2ab99172d0e7f0a1fa291f457733a4d
srodriguezrobayo <113036583+srodriguezrobayo@users.noreply.github.com>
2023-06-27 19:14:48 -0500

    Reserve Shoot proyecto SENA todos los trabajos del tercer trimestre

--- Tercer_trimestre/Tercer_trimestre.py
+++ Tercer_trimestre/Tercer_trimestre.py
@@ -1,3 +1,2 @@
 #Reserve Shoot proyecto SENA todos los trabajos del tercer trimestre
 print("Aqui van todos los trabajos del tercer trimestre del proyecto")
-print("Aqui van todos los trabajos del tercer trimestre del proyecto")
