f71581b80de90adc370cc410ddab3e65c806f6d3
srodriguezrobayo <113036583+srodriguezrobayo@users.noreply.github.com>
2023-06-27 19:14:28 -0500

    Update Tercer_trimestre.py

--- Tercer_trimestre/Tercer_trimestre.py
+++ Tercer_trimestre/Tercer_trimestre.py
@@ -1,2 +1,3 @@
 #Reserve Shoot proyecto SENA todos los trabajos del tercer trimestre
 print("Aqui van todos los trabajos del tercer trimestre del proyecto")
+print("Aqui van todos los trabajos del tercer trimestre del proyecto")
